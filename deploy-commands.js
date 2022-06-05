ec47e7de45375e0d702c3273d8fb782424162962
Josh Thurston <Joshthurston92@gmail.com>
2022-06-04 18:45:56 -0600

    added slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,6 +12,12 @@ const commands = [
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 	new SlashCommandBuilder().setName('shop').setDescription('Replies with item shop'),
+	new SlashCommandBuilder().setName('d20').setDescription('Roll a 20 sided dice'),
+	new SlashCommandBuilder().setName('d12').setDescription('Roll a 12 sided dice'),
+	new SlashCommandBuilder().setName('d10').setDescription('Roll a 10 sided dice'),
+	new SlashCommandBuilder().setName('d8').setDescription('Roll a 8 sided dice'),
+	new SlashCommandBuilder().setName('d6').setDescription('Roll a 6 sided dice'),
+	new SlashCommandBuilder().setName('d4').setDescription('Roll a 4 sided dice'),
 ]
 	.map(command => command.toJSON());
 
